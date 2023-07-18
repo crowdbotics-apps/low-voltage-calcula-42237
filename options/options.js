9027ed015baf3be4745e8666e9a6575f5338534e
Crowdbotics <team@crowdbotics.com>
2023-07-18 01:48:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "low-voltage-calcula-42237",
+  url: "https://low-voltage-calcula-42237.botics.co",
+  api: "https://low-voltage-calcula-42237.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
